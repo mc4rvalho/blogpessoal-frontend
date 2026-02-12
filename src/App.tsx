c1cf83f704f1e124743c7b672f1b9a6b6d08953b
mc4rvalho <mcarvalho093@gmail.com>
2026-02-12 19:06:00 -0300

    Criando Component Página de Login

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Home } from "./pages/home/Home";
 import { Footer } from "./components/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Cadastro } from "./pages/cadastro/Cadastro";
+import { Login } from "./pages/login/Login";
 
 export function App() {
   return (
@@ -11,8 +12,9 @@ export function App() {
         <Navbar />
         <div className="min-h-[80vh]">
           <Routes>
+            <Route path="/" element={<Login />} />
             <Route path="/home" element={<Home />} />
-            <Route path="/" element={<Cadastro />} />
+            <Route path="/cadastro" element={<Cadastro />} />
           </Routes>
         </div>
         <Footer />
