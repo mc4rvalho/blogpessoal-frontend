e4f0345466787ef37afd487e69d63ad8e251ac43
mc4rvalho <mcarvalho093@gmail.com>
2026-02-12 18:22:52 -0300

    Adicionando react router dom e rota para home

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/home/Home";
 import { Footer } from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
