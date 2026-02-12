1db88795c7fccbf3ffe62ebd66283e204c7f3db2
mc4rvalho <mcarvalho093@gmail.com>
2026-02-12 18:52:19 -0300

    Criando Component Cadastro de Usuário

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/home/Home";
 import { Footer } from "./components/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Cadastro } from "./pages/cadastro/Cadastro";
 
 export function App() {
   return (
@@ -10,8 +11,8 @@ export function App() {
         <Navbar />
         <div className="min-h-[80vh]">
           <Routes>
-            <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Cadastro />} />
           </Routes>
         </div>
         <Footer />
