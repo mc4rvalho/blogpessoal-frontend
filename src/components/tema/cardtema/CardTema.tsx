9dec10fa4afc3562caa4a5b4f2deb5364549e4bc
mc4rvalho <mcarvalho093@gmail.com>
2026-02-19 20:45:24 -0300

    chore: create logic FormTema.

--- src/components/tema/cardtema/CardTema.tsx
+++ src/components/tema/cardtema/CardTema.tsx
@@ -15,7 +15,7 @@ export function CardTema({tema}: CardTemaProps) {
 
       <div className="flex">
         <Link
-          to=""
+          to={`/editartema/${tema.id}`}
           className="w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2"
         >
           <button>Editar</button>
