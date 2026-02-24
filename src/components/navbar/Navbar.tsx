c13185f0153dd481fe34a0fee6155b95a69e570f
mc4rvalho <mcarvalho093@gmail.com>
2026-02-23 23:14:21 -0300

    chore: add Perfil & Logic

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -22,12 +22,18 @@ export function Navbar() {
           </Link>
 
           <div className="flex gap-4">
-            <Link to='/postagens' className="hover:underline">Postagens</Link>
+            <Link to="/postagens" className="hover:underline">
+              Postagens
+            </Link>
             <Link to="/temas" className="hover:underline">
               Temas
             </Link>
-             <Link to='/cadastrartema' className="hover:underline">Cadastrar tema</Link>
-             Perfil
+            <Link to="/cadastrartema" className="hover:underline">
+              Cadastrar tema
+            </Link>
+            <Link to="/perfil" className="hover:underline">
+              Perfil
+            </Link>
             <Link to="" onClick={logout} className="hover:underline">
               Sair
             </Link>
---
+++ src/pages/perfil/Perfil.tsx
@@ -0,0 +1,40 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { AuthContext } from "../../contexts/AuthContext";
+
+export function Perfil() {
+  const navigate = useNavigate();
+
+  const { usuario } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (usuario.token === "") {
+      alert("Você precisa estar logado");
+      navigate("/");
+    }
+  }, [usuario.token, navigate]);
+
+  return (
+    <div className="mx-4 flex justify-center">
+      <div className="container mx-auto my-4 overflow-hidden rounded-2xl">
+        <img
+          className="h-72 w-full border-b-8 border-white object-cover"
+          src="https://i.imgur.com/ZZFAmzo.jpg"
+          alt="Capa do Perfil"
+        />
+
+        <img
+          className="relative z-10 mx-auto mt-[-8rem] w-56 rounded-full border-8 border-white"
+          src={usuario.foto}
+          alt={`Foto de perfil de ${usuario.nome}`}
+        />
+
+        <div className="relative mt-[-6rem] flex h-72 flex-col items-center justify-center bg-sky-500 text-2xl text-white">
+          <p>Nome: {usuario.nome} </p>
+          <p>Email: {usuario.usuario}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
